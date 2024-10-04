efade0ef92a293d00c8000c489b683d079ad468f
hkmtakgn <hikmetakgun94@gmail.com>
2024-10-05 00:55:31 +0300

    'guncelleme-yapildi'

--- PYTHON/3-kesme isaretleri ve tirnak kullanımı/3-kesme-isaretleri-ve-tirnak-kullanımı.py
+++ PYTHON/3-kesme isaretleri ve tirnak kullanımı/3-kesme-isaretleri-ve-tirnak-kullanımı.py
@@ -38,7 +38,7 @@ print(
 # print("Merhaba Hikmet "Togg" nasıldı?") hatalı kullanım
 # çift tırnak içinde çift tırnak kullanılamaz.
 # bunun için doğru kullanım aşağıdaki gibi olmalı
-print("Merhaba Hikmet \"Togg\" nasıldı?") 
+print("Merhaba Hikmet \"Fringe\" nasıldı?") 
 # yani "\" kullanımı ile hemen sonrasındakini etkisiz eleman moduna alırız
 # print('Hikmet sen 'Fringe' dizisini izledin mi?') yanlış kullanım
 print('Hikmet sen \'Fringe\' dizisini izledin mi?')
@@ -48,3 +48,12 @@ print("Hikmet\tAKGÜN")
 print("Hikmet\t\tAKGÜN")
 print("Hikmet\t\t\tAKGÜN")
 # print("Hikmet\tAKGÜN") "\t" ile tab tuşuna basarmış gibi bir tab boşluk bırakırız
+print(
+    """
+    Merhaba
+    Hikmet
+    Fringe dizisini
+    beğendin mi?
+    """
+)
+print("evet beğendim.\"Fringe\" güzel diziydi!")
