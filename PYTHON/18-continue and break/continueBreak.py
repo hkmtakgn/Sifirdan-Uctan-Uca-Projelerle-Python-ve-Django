e6592a5653514233ae7c335ece172b57cb7bf8d5
hkmtakgn <hikmetakgun94@gmail.com>
2024-10-18 09:03:19 +0300

    break-guncellemesi

--- PYTHON/18-continue and break/continueBreak.py
+++ PYTHON/18-continue and break/continueBreak.py
@@ -1,3 +1,22 @@
+# names = ["Sena","Sinem","Varşin","Hikmet","Derin","Asude","Ali","Mehmet"]
+
+# for name in names:
+#     if name == "Derin":
+#         continue
+#     elif name == "Hikmet":
+#         print(f"{name} - Admin")
+#     else:
+#         print(f"{name} - Standart kullanıcı")
+
+# # çıktısı aşağıdaki gibi olacaktır ve "Derin" ismini atlayacaktır
+# # Sena - Standart kullanıcı
+# # Sinem - Standart kullanıcı
+# # Varşin - Standart kullanıcı
+# # Hikmet - Admin
+# # Asude - Standart kullanıcı
+# # Ali - Standart kullanıcı
+# # Mehmet - Standart kullanıcı
+
 names = ["Sena","Sinem","Varşin","Hikmet","Derin","Asude","Ali","Mehmet"]
 
 for name in names:
@@ -5,15 +24,13 @@ for name in names:
         continue
     elif name == "Hikmet":
         print(f"{name} - Admin")
+        break
     else:
         print(f"{name} - Standart kullanıcı")
 
-# çıktısı aşağıdaki gibi olacaktır ve "Derin" ismini atlayacaktır
+#"Derin" ismini atlayacaktır ve "Hikmet" varsa "Admin" ekledikten sonra duracaktır
+# çıktısı aşağıdaki gibi 
 # Sena - Standart kullanıcı
 # Sinem - Standart kullanıcı
 # Varşin - Standart kullanıcı
 # Hikmet - Admin
-# Asude - Standart kullanıcı
-# Ali - Standart kullanıcı
-# Mehmet - Standart kullanıcı
-
