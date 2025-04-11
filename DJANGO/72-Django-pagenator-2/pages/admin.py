c9c445fbc5a0d91859c2387625fd14d567c2c633
hkmtakgn <hikmetakgun94@gmail.com>
2025-04-11 04:29:10 +0300

    Django-pagenator-2

---
+++ DJANGO/72-Django-pagenator-2/pages/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from pages.models import Page,Developer
+
+# Register your models here.
+
+@admin.register (Page)
+class PageAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register (Developer)
+class DevAdmin (admin.ModelAdmin):
+    pass
+
