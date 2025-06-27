80a30517ec6be015cffcb27429ff5617491072b5
Hikmet AKGÜN <113580325+hkmtakgn@users.noreply.github.com>
2025-06-27 18:35:23 +0300

    89-Django-forms-username-validation-2

---
+++ DJANGO/89-Django-forms-username-validation-2/my_weblog/forms.py
@@ -0,0 +1,39 @@
+from django import forms
+from my_weblog.models import PostModel
+
+#2. Yöntem ↓
+from django.core import validators
+
+
+class PostForm(forms.ModelForm):
+    #2.Yöntem ↓
+    username = forms.CharField(validators=[
+        validators.MinLengthValidator(3, "Lütfen en az 3 karakter giriniz!")
+    ])
+
+    class Meta:
+        model = PostModel
+        fields = [
+            "username",
+            "content",
+            "is_active",
+            "post_img",
+        ]
+
+        # exclude = [
+        #     "is_active",
+        # ]
+
+        widgets = {
+            "username":
+            forms.TextInput(attrs={"placeholder": "Enter your username !"}),
+            "content":
+            forms.Textarea(attrs={"placeholder": "Write something !"})
+        }
+
+    # 1.Yöntem ↓
+    # def clean_username(self):
+    #     username = self.cleaned_data.get("username")
+    #     if len(username) < 3:
+    #         raise forms.ValidationError('Lütfen en az 3 karakter giriniz!')
+    #     return username
